0796b8e90ecfb6c4454d1427ad2562ff5585529c
Hugo Mok <hugo.mok@infocast.com.hk>
2025-04-09 16:06:55 +0800

    typo frontend

--- frontend/web/app/page.tsx
+++ frontend/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const data = await fetch(`${process.env.SCH_HOST}/schedule`, { method: 'POST' })
-  console.log(await data.json());
+  // const data = await fetch(`${process.env.SCH_HOST}/schedule`, { method: 'POST' })
+  // console.log(await data.json());
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
