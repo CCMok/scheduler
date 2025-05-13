3bc2b11f09eca02c845f171fda986bd8510c1d2e
Hugo Mok <hugo.mok@infocast.com.hk>
2025-05-13 18:10:56 +0800

    login page

---
+++ frontend/web/app/login/page.tsx
@@ -0,0 +1,49 @@
+import { ThemeToggle } from '@/components/root/buttons/theme-toggle';
+import { AnimatedGridPattern } from '@/magicui/animated-grid-pattern';
+import { Button } from '@/shadcn/components/ui/button';
+import { Input } from '@/shadcn/components/ui/input';
+import { cn } from '@/shadcn/libs/utils';
+
+export default function LoginPage() {
+  return (
+    <div className='flex min-h-screen'>
+      <div className='absolute top-4 right-4'>
+        <ThemeToggle />
+      </div>
+      <section className='hidden md:block w-1/2 relative bg-neutral-950 border-r'>
+        <AnimatedGridPattern
+          numSquares={30}
+          maxOpacity={0.1}
+          duration={3}
+          repeatDelay={1}
+          className={cn(
+            '[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]',
+            'inset-y-[30%] skew-y-12 h-1/2',
+          )}
+        />
+        <p className='text-lg italic absolute bottom-8 left-8 text-white'>
+          讓計劃更有效率
+        </p>
+      </section>
+      <section className='w-full md:w-1/2 flex flex-col justify-center items-center px-8 space-y-6'>
+        <div className='space-y-2 flex flex-col items-center'>
+          <h1 className='text-2xl font-bold'>登入</h1>
+          <p className='text-secondary-foreground'>
+            輸入您的電子郵件地址
+          </p>
+        </div>
+        <form className='w-full max-w-sm space-y-4 flex flex-col'>
+          <Input
+            type='email'
+            placeholder='name@example.com'
+          />
+          <Button
+            type='submit'
+          >
+            使用電子郵件登入
+          </Button>
+        </form>
+      </section>
+    </div>
+  );
+}
--- frontend/web/app/page.tsx
+++ frontend/web/app/page.tsx
@@ -1,19 +1,23 @@
-import { ThemeToggle } from "@/components/root/buttons/theme-toggle";
-import { Button } from "@/shadcn/components/ui/button";
+import { ThemeToggle } from '@/components/root/buttons/theme-toggle';
+import { Path } from '@/libs/share/enums/path';
+import { Button } from '@/shadcn/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="absolute top-4 right-4">
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <div className='absolute top-4 right-4'>
         <ThemeToggle />
       </div>
-      <div className="text-center p-8">
-        <h1 className="text-4xl font-bold mb-4">歡迎使用 Scheduler</h1>
-        <p className="text-lg text-secondary-foreground mb-6">
-          簡化您的日程安排並有效率地管理您的時間。
+      <div className='text-center px-8'>
+        <h1 className='text-4xl font-bold'>歡迎使用 Scheduler</h1>
+        <p className='text-lg text-secondary-foreground mt-4'>
+          簡化您的日程安排並有效率地管理您的團隊。
         </p>
-        <Button className="px-6 py-3">
-          開始使用
+        <Button className='mt-6' asChild>
+          <Link href={Path.LOGIN}>
+            開始使用
+          </Link>
         </Button>
       </div>
     </div>
