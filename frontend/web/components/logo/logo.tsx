6927ec1c50bc54254f1a3d89a110cf43265a7226
Hugo Mok <hugo.mok@infocast.com.hk>
2025-05-14 12:14:54 +0800

    logo redirect home

--- frontend/web/components/logo/logo.tsx
+++ frontend/web/components/logo/logo.tsx
@@ -1,19 +1,36 @@
+import { cn } from "@/external/shadcn/libs/utils";
+import { Path } from "@/libs/share/_general/enums/path";
 import { ClassNameProps } from "@/libs/share/_general/props/class-name-props";
 import { Leckerli_One } from "next/font/google";
+import Link from "next/link";
 
 const leckerliOne = Leckerli_One({ weight: '400', subsets: ["latin"] });
 
+type Props = ClassNameProps & {
+  redirectHome?: boolean;
+}
+
 export default function Logo({
   className,
-}: Readonly<ClassNameProps>) {
+  redirectHome = true,
+}: Readonly<Props>) {
   return (
-    <p className={`space-x-2 flex items-center ${className}`}>
+    <Link
+      href={Path.HOME}
+      className={cn(
+        'space-x-2 flex items-center',
+        !redirectHome && 'pointer-events-none',
+        className,
+      )}
+      aria-disabled={redirectHome ? undefined : true}
+      tabIndex={-1}
+    >
       <span className={`${leckerliOne.className} text-2xl`}>
         S
       </span>
       <span className={`font-medium text-lg`}>
         Scheduler
       </span>
-    </p>
+    </Link>
   )
 }
