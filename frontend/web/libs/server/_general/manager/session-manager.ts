a7c0decdf8f3d8f87ef2daecb3181b19869b3da7
Hugo Mok <hugo.mok@infocast.com.hk>
2025-05-21 12:18:28 +0800

    access control in middleware

--- frontend/web/libs/server/_general/manager/session-manager.ts
+++ frontend/web/libs/server/_general/manager/session-manager.ts
@@ -1,26 +1,49 @@
 import 'server-only'
-import { issueToken } from "./jwt-manager";
+import { issueToken, TokenPayload, verifyToken } from "./jwt-manager";
 import { UserRole } from "../../user/models/user-models";
 import { SessionPayload } from "../models/session-payload";
-import { setCookie } from './cookie-manager';
+import { deleteCookie, getCookie, setCookie } from './cookie-manager';
 
 // config
 const sessionExpirationTime = '1h';
 const cookieName = 'token';
 
 export const setSession = async (userRole: UserRole): Promise<void> => {
-  const sessionPayload = getSessionPayload(userRole);
+  const sessionPayload = getSessionPayloadFromUserRole(userRole);
 
   const token = await issueToken(sessionPayload, sessionExpirationTime);
 
   await setCookie(cookieName, token);
 }
 
-const getSessionPayload = (userRole: UserRole): SessionPayload => {
+export const getSession = async (): Promise<SessionPayload | undefined> => {
+  const token = await getCookie(cookieName)
+  if (!token) return;
+
+  const tokenPayload = await verifyToken(token)
+  if (!tokenPayload) return;
+
+  return getSessionPayloadFromTokenPayload(tokenPayload);
+}
+
+export const deleteSession = async (): Promise<void> => {
+  await deleteCookie(cookieName);
+}
+
+const getSessionPayloadFromUserRole = (userRole: UserRole): SessionPayload => {
   return {
     userId: userRole.id,
     email: userRole.email,
     roleEnum: userRole.role.enum,
     name: userRole.name ?? undefined,
+  }
+}
+
+const getSessionPayloadFromTokenPayload = (tokenPayload: TokenPayload): SessionPayload | undefined => {
+  return {
+    userId: tokenPayload.userId,
+    email: tokenPayload.email,
+    roleEnum: tokenPayload.roleEnum,
+    name: tokenPayload.name,
   }
 }
--- frontend/web/middleware.ts
+++ frontend/web/middleware.ts
@@ -1,20 +1,51 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Path } from './libs/share/_general/enums/path'
+import { deleteSession, getSession } from './libs/server/_general/manager/session-manager';
+import { SessionPayload } from './libs/server/_general/models/session-payload';
 
 const publicPaths = [
-  Path.HOME,
   Path.LOGIN,
 ]
+
+const redirectPublicPath = Path.LOGIN;
+const redirectPrivatePath = Path.DASHBOARD;
 
 export default async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
-  const isPublicPath = publicPaths.some(publicPath => publicPath === path);
-  console.log('isPublicPath', isPublicPath)
+  const isPrivatePath = checkIsPrivatePath(path);
+  const sessionPayload = await getSession();
 
-  return NextResponse.next()
+  if (isPrivatePath) {
+    return await handlePrivatePath(request, sessionPayload)
+  }
+
+  return handlePublicPath(request, sessionPayload)
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.ico$|.*\\.png$).*)'],
+}
+
+const checkIsPrivatePath = (path: string): boolean => {
+  if (path === Path.HOME) return false;
+
+  return !publicPaths.some(publicPath => path.startsWith(publicPath));
+}
+
+const handlePrivatePath = async (request: NextRequest, sessionPayload: SessionPayload | undefined): Promise<NextResponse> => {
+  if (sessionPayload) {
+    return NextResponse.next()
+  }
+
+  await deleteSession();
+  return NextResponse.redirect(new URL(redirectPublicPath, request.url))
+}
+
+const handlePublicPath = (request: NextRequest, sessionPayload: SessionPayload | undefined): NextResponse => {
+  if (sessionPayload) {
+    return NextResponse.redirect(new URL(redirectPrivatePath, request.url))
+  }
+
+  return NextResponse.next()
 }
