20247c2eeae72f2821a7e5444064573d1f74a1cb
Hugo Mok <hugo.mok@infocast.com.hk>
2025-05-26 17:34:33 +0800

    blank new page. roster, setting

---
+++ frontend/web/app/(private)/roster/page.tsx
@@ -0,0 +1,5 @@
+export default function RosterPage() {
+  return (
+    <div>Roster page</div>
+  )
+}
---
+++ frontend/web/app/(private)/setting/page.tsx
@@ -0,0 +1,5 @@
+export default function SettingPaege() {
+  return (
+    <div>Setting page</div>
+  )
+}
