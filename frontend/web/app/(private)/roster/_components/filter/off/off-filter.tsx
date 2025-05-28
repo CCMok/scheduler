4cd868d4e8cb43a06ac78082a41b9d566dc8b9f7
Hugo Mok <hugo.mok@infocast.com.hk>
2025-05-28 18:15:25 +0800

    off array remove button card

--- frontend/web/app/(private)/roster/_components/filter/off/off-filter.tsx
+++ frontend/web/app/(private)/roster/_components/filter/off/off-filter.tsx
@@ -6,55 +6,74 @@ import { Button } from "@/external/shadcn/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/external/shadcn/components/ui/card"
 import { FormField } from "@/external/shadcn/components/ui/form";
 import { RosterFilterFormInput } from "@/libs/client/roster/models/roster-filter-form-input"
-import { Plus } from "lucide-react";
-import { useFieldArray, useFormContext } from "react-hook-form"
+import { Minus, Plus } from "lucide-react";
+import { useMemo } from "react";
+import { useFieldArray, useFormContext, useWatch } from "react-hook-form"
 
 export default function OffFilter() {
   const { control, setValue } = useFormContext<RosterFilterFormInput>();
 
-  // TODO apply remove
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'offs',
   })
 
-  const onClickAdd = () => {
-    append({
-      worker_id: '',
-      days: [],
-    })
-  }
+  const departmentId = useWatch({
+    control,
+    name: 'departmentId',
+    defaultValue: '', // TODO
+  })
+
+  useMemo(() => { }, [])
+
+  const onClickAppend = () => append({
+    worker_id: '',
+    days: [],
+  })
+
+  const onClickRemove = (index: number) => remove(index)
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>請假</CardTitle>
       </CardHeader>
-      <CardContent className='flex flex-wrap gap-4'>
+      <CardContent className='space-y-4'>
         {fields.map((item, index) => (
-          <FormField
-            key={item.id}
-            control={control}
-            name={`offs.${index}.worker_id`}
-            render={({ field }) => (
-              <CustomFormItem label='人員'>
-                <ComboBox
-                  value={field.value}
-                  // TODO: real data
-                  items={[{ id: 1, name: 'Worker 1' }, { id: 2, name: 'Worker 2' }]}
-                  getValue={item => item.id.toString()}
-                  getDisplayName={item => item.name}
-                  onSelect={value => setValue(`offs.${index}.worker_id`, value)}
-                />
-              </CustomFormItem>
-            )}
-          />
+          <Card key={item.id} className='w-fit'>
+            <CardContent className='flex gap-4 items-center'>
+              <FormField
+                control={control}
+                name={`offs.${index}.worker_id`}
+                render={({ field }) => (
+                  <CustomFormItem label='人員'>
+                    <ComboBox
+                      value={field.value}
+                      // TODO: real data
+                      items={[{ id: 1, name: 'Worker 1' }, { id: 2, name: 'Worker 2' }]}
+                      getValue={item => item.id.toString()}
+                      getDisplayName={item => item.name}
+                      onSelect={value => setValue(`offs.${index}.worker_id`, value)}
+                    />
+                  </CustomFormItem>
+                )}
+              />
+              <Button
+                type='button'
+                variant='secondary'
+                size='icon'
+                onClick={() => onClickRemove(index)}
+              >
+                <Minus />
+              </Button>
+            </CardContent>
+          </Card>
         ))}
         <Button
           type='button'
           variant='outline'
           size='icon'
-          onClick={onClickAdd}
+          onClick={onClickAppend}
         >
           <Plus />
         </Button>
