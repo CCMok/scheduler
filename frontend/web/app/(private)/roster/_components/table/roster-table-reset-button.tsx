f8e6d62f38e6bb4f5cfa69d0bbd9a661824a0983
Hugo Mok <hugo.mok@infocast.com.hk>
2025-07-17 11:15:05 +0800

    Warning dialog apply to rosterTableResetButton

--- frontend/web/app/(private)/roster/_components/table/roster-table-reset-button.tsx
+++ frontend/web/app/(private)/roster/_components/table/roster-table-reset-button.tsx
@@ -1,50 +1,30 @@
 'use client';
 
-import LoadingButton from "@/components/button/loading-button";
 import { useArrangeRosterStore } from "@/components/store/roster/arrange/arrange-roster-store-provider";
 import { RotateCcw } from "lucide-react";
-import { useState } from "react";
-import { AlertDialogHeader, AlertDialogTrigger, AlertDialogFooter, AlertDialog, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogCancel, AlertDialogAction } from "@/external/shadcn/components/ui/alert-dialog";
+import { AlertDialogTrigger } from "@/external/shadcn/components/ui/alert-dialog";
 import CustomButton from "@/components/button/custom-button";
+import WarningDialog from "@/components/dialog/warning-dialog";
 
 export default function RosterTableResetButton() {
   const initialSchedules = useArrangeRosterStore(state => state.initialSchedules);
   const setModifiedSchedules = useArrangeRosterStore(state => state.setModifiedSchedules);
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  const onClickContinue = () => {
-    setIsLoading(true);
-    reset();
-    setIsLoading(false)
-  }
-
-  const reset = async () => {
+  const onContinue = () =>
     setModifiedSchedules(initialSchedules)
-  }
 
   return (
-    <AlertDialog>
+    <WarningDialog
+      title='確定要重置值班表嗎?'
+      description='重置將會覆蓋現有的值班表，沒有儲存的資料將會遺失，請確認是否繼續。'
+      onContinue={onContinue}
+    >
       <AlertDialogTrigger asChild>
         <CustomButton variant='secondary'>
           <RotateCcw />
           重置
         </CustomButton>
       </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>確定要重置值班表嗎?</AlertDialogTitle>
-          <AlertDialogDescription>
-            重置將會覆蓋現有的值班表，沒有儲存的資料將會遺失，請確認是否繼續。
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>取消</AlertDialogCancel>
-          <AlertDialogAction asChild>
-            <LoadingButton isLoading={isLoading} onClick={onClickContinue}>繼續</LoadingButton>
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    </WarningDialog>
   )
 }
--- frontend/web/components/dialog/warning-dialog.tsx
+++ frontend/web/components/dialog/warning-dialog.tsx
@@ -3,10 +3,11 @@
 import { AlertDialogHeader, AlertDialogFooter, AlertDialog, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogCancel } from "@/external/shadcn/components/ui/alert-dialog";
 import LoadingButton from "@/components/button/loading-button";
 import { Dispatch, SetStateAction, useState } from "react";
+import { ChildrenProps } from "@/libs/share/_general/props/children-props";
 
-type Props = {
-  isOpen: boolean;
-  setIsOpen: Dispatch<SetStateAction<boolean>>;
+type Props = ChildrenProps & {
+  isOpen?: boolean;
+  setIsOpen?: Dispatch<SetStateAction<boolean>>;
   title?: string;
   description?: string;
   onContinue: () => Promise<void> | void;
@@ -18,20 +19,26 @@ export default function WarningDialog({
   title,
   description,
   onContinue,
+  children,
 }: Readonly<Props>) {
   const [isLoading, setIsLoading] = useState(false)
+  
+  const [isOpenDefault, setIsOpenDefault] = useState(false)
+  const isOpenFinal = isOpen ?? isOpenDefault
+  const setIsOpenFinal = setIsOpen ?? setIsOpenDefault
 
   const onClickContinue = async () => {
     setIsLoading(true)
 
     await onContinue();
 
-    setIsOpen(false)
+    setIsOpenFinal(false)
     setIsLoading(false)
   }
 
   return (
-    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+    <AlertDialog open={isOpenFinal} onOpenChange={setIsOpenFinal}>
+      {children}
       <AlertDialogContent>
         <AlertDialogHeader>
           {title && <AlertDialogTitle>{title}</AlertDialogTitle>}
