356f964717418e58efd142ea975ff541f32c2559
Hugo Mok <hugo.mok@infocast.com.hk>
2025-07-29 17:12:37 +0800

    Add comment for vercel prisma monorepos workaround

--- frontend/web/next.config.ts
+++ frontend/web/next.config.ts
@@ -5,6 +5,7 @@ const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
     if (isServer) {
+      // Workaround for Vercel + Prisma + Monorepos (Multi app in 1 repo) issue (https://www.prisma.io/docs/orm/prisma-client/deployment/serverless/deploy-to-vercel)
       config.plugins = [...config.plugins, new PrismaPlugin()]
     }
 
