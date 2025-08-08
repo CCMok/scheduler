71bab816e01fd7e42aec4c34799ebad86663403f
Hugo Mok <hugo.mok@infocast.com.hk>
2025-08-08 11:10:48 +0800

    Fix next build engine cannot auto determine the route is dynamic or static generation

--- frontend/web/libs/server/_general/services/general-service.ts
+++ frontend/web/libs/server/_general/services/general-service.ts
@@ -1,6 +1,7 @@
 import { ServiceResponse } from "@/libs/share/_general/models/service-response";
 import { ServiceResponseStatus } from "../../../share/_general/enums/service-response-status";
 import { ZodError } from "zod";
+import { DynamicServerError } from "next/dist/client/components/hooks-server-context";
 
 export const serviceWrapper = async <T>(service: () => ServiceResponse<T> | Promise<ServiceResponse<T>>): Promise<ServiceResponse<T>> => {
   try {
@@ -11,6 +12,13 @@ export const serviceWrapper = async <T>(service: () => ServiceResponse<T> | Prom
 }
 
 const handleError = <T>(error: unknown): ServiceResponse<T> => {
+  // Prevent dynamicServerError is uncatch by nextjs build engine
+  // dynamicServerError is for nextjs to detemine route is dynamic or static generation. So it is expected error
+  // e.g. call await cookies()
+  if (error instanceof DynamicServerError) {
+    throw error;
+  }
+
   if (error instanceof ZodError) {
     console.warn('Invalid request', error.errors)
     return {
