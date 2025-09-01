7aea5700865a5a32e6971e849ed5e1f3f20101f1
Hugo Mok <hugo.mok@infocast.com.hk>
2025-09-01 14:31:50 +0800

    action dropdown menu

---
+++ frontend/web/components/dialog/delete-dialog.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import WarningDialog from '@/components/dialog/warning-dialog';
+import { toast } from 'sonner';
+import { SONNER_DEFAULT_OPTIONS } from '@/libs/client/_general/constants/sonnar-constant';
+import { UiMessageTitle } from '@/libs/share/_general/enums/ui-message';
+import { useRouter } from 'next/navigation';
+import { handleServiceResponse } from '@/libs/share/_general/utils/service-response-handler';
+import { ServiceResponse } from '@/libs/share/_general/models/service-response';
+
+type Props = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  entityName?: string;
+  displayName?: string;
+  submit?: () => Promise<ServiceResponse>;
+};
+
+export default function DeleteDialog({
+  isOpen,
+  setIsOpen,
+  entityName = '',
+  displayName = '',
+  submit,
+}: Readonly<Props>) {
+  const router = useRouter();
+
+  const onContinue = async () => {
+    if (!submit) return
+
+    const response = await submit();
+    const uiResponse = handleServiceResponse(response, path => router.push(path));
+    if (!uiResponse.isSuccess) {
+      toast.error(uiResponse.message.title, {
+        ...SONNER_DEFAULT_OPTIONS,
+        description: uiResponse.message.content,
+      })
+      return
+    }
+
+    toast.success('刪除' + UiMessageTitle.SUCCESS, {
+      ...SONNER_DEFAULT_OPTIONS,
+      description: `${entityName}已刪除`,
+    })
+
+    router.refresh()
+    setIsOpen(false)
+  }
+
+  return (
+    <WarningDialog
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      title={`刪除${entityName}`}
+      description={`確定要刪除 ${displayName} 嗎？`}
+      onContinue={onContinue}
+    />
+  )
+}
