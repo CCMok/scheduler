e8cf513acbb41d976584c2e5150ea60fe31b8812
Hugo Mok <hugo.mok@infocast.com.hk>
2025-09-03 10:49:55 +0800

    Worker id page

---
+++ frontend/web/app/(private)/setting/worker/[id]/_components/update-name/update-worker-name-section.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Worker } from "@/external/prisma-generated"
+import UpdateNameLayout from "@/components/layout/update-name/update-name-layout"
+import { UpdateNameFormInput } from "@/libs/client/setting/models/update-name-form-input"
+import { ServiceResponse } from "@/libs/share/_general/models/service-response"
+import { UpdateWorkerNameRequest } from "@/libs/server/worker/models/update-worker-name-request"
+import { updateWorkerNameAction } from "@/libs/server/worker/actions/update-worker-name-action"
+
+type Props = {
+  worker: Worker;
+}
+
+export default function UpdateWorkerNameSection({
+  worker,
+}: Readonly<Props>) {
+  const submit = async (input: UpdateNameFormInput): Promise<ServiceResponse> => {
+    const request: UpdateWorkerNameRequest = {
+      id: worker.id,
+      name: input.name,
+    }
+
+    return await updateWorkerNameAction(request)
+  }
+
+  return (
+    <UpdateNameLayout
+      entityName="人員"
+      originalName={worker.name}
+      submit={submit}
+    />
+  )
+}
--- frontend/web/app/(private)/setting/worker/[id]/page.tsx
+++ frontend/web/app/(private)/setting/worker/[id]/page.tsx
@@ -2,22 +2,18 @@ import { ParamProps } from "@/libs/share/_general/props/param-props";
 import { Param } from "@/libs/share/_general/enums/param";
 import { notFound, redirect } from "next/navigation";
 import { fetchData } from "@/libs/share/_general/utils/fetch";
-import { WorkerPosts } from "@/libs/server/worker/models/worker-dao";
-import { GetWorkerPostsRequest } from "@/libs/server/worker/models/get-worker-posts-request";
-import { getWorkerPostsService } from "@/libs/server/worker/services/get-worker-posts-service";
-import WorkerUpdateNameSection from "./_components/name/worker-update-name-section";
-import WorkerPostTableSection from "./_components/table/worker-post-table-section";
-import { WorkerUpdateStoreProvider } from "./_components/store/worker-update-store-provider";
-import BackHeader from "@/components/header/back-header";
+import IndividualSettingLayout from "@/components/layout/setting/individual-setting-layout";
+import { Worker } from "@/external/prisma-generated";
+import { getWorkersService } from "@/libs/server/worker/services/get-workers-service";
+import UpdateWorkerNameSection from "./_components/update-name/update-worker-name-section";
 
-const getWorkerPosts = async (id: number): Promise<WorkerPosts | undefined> => {
-  const request: GetWorkerPostsRequest = { id }
-
-  return await fetchData(
-    async () => getWorkerPostsService(request),
+const getWorkerPosts = async (id: number): Promise<Worker | undefined> => {
+  const workers = await fetchData(
+    async () => getWorkersService({ where: { id } }),
     path => redirect(path),
-    undefined,
+    [],
   )
+  return workers[0]
 }
 
 type Props = ParamProps<{ [Param.ID]: string }>
@@ -29,23 +25,15 @@ export default async function WorkerEditPage({
   const id = Number(paramId);
   if (isNaN(id)) notFound();
 
-  const workerPosts = await getWorkerPosts(id);
-  if (!workerPosts) notFound()
+  const worker = await getWorkerPosts(id);
+  if (!worker) notFound()
 
   return (
-    <WorkerUpdateStoreProvider initState={{
-      workerId: id,
-      workerName: workerPosts.name,
-      posts: workerPosts.posts,
-      departmentId: workerPosts.departmentId,
-    }}>
-      <div className="space-y-4">
-        <BackHeader>
-          <span>{workerPosts.name}</span>
-        </BackHeader>
-        <WorkerUpdateNameSection />
-        <WorkerPostTableSection />
-      </div>
-    </WorkerUpdateStoreProvider>
+    <IndividualSettingLayout
+      title={worker.name}
+      updateNameSection={<UpdateWorkerNameSection worker={worker} />}
+      // TODO
+      // otherSection={<ChildrenSection departmentId={id} />}
+    />
   )
 }
