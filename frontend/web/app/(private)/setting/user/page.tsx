fdec0a4926f2b25d3020cfba75e2edd1abfc7d92
Hugo Mok <hugomok0920@gmail.com>
2025-09-13 19:31:24 +0800

    add header at user setting

--- frontend/web/app/(private)/setting/user/page.tsx
+++ frontend/web/app/(private)/setting/user/page.tsx
@@ -3,15 +3,16 @@ import UpdatePasswordForm from "./_components/update-password-form";
 import UpdateUserNameForm from "./_components/update-user-name-form";
 import { redirect } from "next/navigation";
 import { REDIRECT_PUBLIC_PATH } from "@/libs/share/_general/utils/path";
+import BreadcrumbHeaderLayout from "@/components/_general/layout/setting/breadcrumb-header-layout";
 
 export default async function UserSettingPage() {
   const session = await getSession();
   if (!session) return redirect(REDIRECT_PUBLIC_PATH);
 
   return (
-    <div className='space-y-4'>
+    <BreadcrumbHeaderLayout current='用戶'>
       <UpdatePasswordForm />
       <UpdateUserNameForm userName={session.name ?? ''} />
-    </div>
+    </BreadcrumbHeaderLayout>
   )
 }
