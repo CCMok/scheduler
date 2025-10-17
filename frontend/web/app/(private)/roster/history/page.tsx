1c936e1a05049315302b73cca0eb5fd8035fcf06
Hugo Mok <hugo.mok@infocast.com.hk>
2025-10-17 11:23:33 +0800

    Roster history page, get request prelim design

--- frontend/web/app/(private)/roster/history/page.tsx
+++ frontend/web/app/(private)/roster/history/page.tsx
@@ -1,4 +1,7 @@
+import CustomCard from "@/components/_general/card/custom-card";
 import SidebarInsetLayout from "@/components/_general/layout/sidebar-inset/sidebar-inset-layout";
+import TableSkeleton from "@/components/_general/skeleton/table-skeleton";
+import { Suspense } from "react";
 
 export default function RosterHistoryPage() {
   return (
@@ -12,9 +15,12 @@ export default function RosterHistoryPage() {
         label: '紀錄',
       },
     ]}>
-      <div>
-        <h1>Roster History</h1>
-      </div>
+      <CustomCard>
+        {/* <OrganizationFilter button={<CreateOrganizationButton />} /> */}
+        <Suspense fallback={<TableSkeleton />}>
+          {/* <OrganizationTableServer role={role} /> */}
+        </Suspense>
+      </CustomCard>
     </SidebarInsetLayout>
   )
 }
