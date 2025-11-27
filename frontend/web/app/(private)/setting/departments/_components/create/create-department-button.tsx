80b2a84a82a2efc6538c7d5420d4cfeffb74f007
Hugo Mok <hugo.mok@infocast.com.hk>
2025-11-27 17:35:14 +0800

    Dialog refactor

--- frontend/web/app/(private)/setting/departments/_components/create/create-department-button.tsx
+++ frontend/web/app/(private)/setting/departments/_components/create/create-department-button.tsx
@@ -1,16 +1,10 @@
 'use client'
 
 import CustomButton from "@/components/_general/button/custom-button";
-import ComboBox from "@/components/_general/combobox/combo-box";
-import CustomDialog from "@/components/_general/dialog/custom-dialog";
-import CustomLink from "@/components/_general/link/custom-link";
 import { Organization } from "@/external/prisma-generated";
-import { AlertDialogTrigger } from "@/external/shadcn/components/ui/alert-dialog";
-import { Label } from "@/external/shadcn/components/ui/label";
-import { PATH } from "@/libs/_general/enums/path";
-import { isNil } from "lodash";
 import { Plus } from "lucide-react";
 import { use, useState } from "react";
+import CreateDepartmentDialog from "./create-department-dialog";
 
 type Props = {
   organizationsPromise: Promise<Organization[]>;
@@ -21,35 +15,23 @@ export default function CreateDepartmentButton({
 }: Readonly<Props>) {
   const organizations = use(organizationsPromise);
 
-  const [organizationId, setOrganizationId] = useState<number | undefined>(undefined);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const onClick = () => setIsDialogOpen(true)
 
   return (
-    <CustomDialog
-      title='新增部門'
-      trigger={(
-        <AlertDialogTrigger asChild>
-          <CustomButton>
-            <Plus />
-            新增
-          </CustomButton>
-        </AlertDialogTrigger>
-      )}
-      submitButton={
-        <CustomButton>
-          <CustomLink href={isNil(organizationId) ? '' : PATH.setting.organizations.departments.new(organizationId)}>
-            繼續
-          </CustomLink>
-        </CustomButton>
-      }
-    >
-      <Label>機構</Label>
-      <ComboBox
-        value={organizationId?.toString() ?? ''}
-        options={organizations}
-        getValue={option => option.id.toString()}
-        getDisplayName={option => option.name}
-        onValueChange={value => setOrganizationId(Number(value))}
+    <>
+      <CustomButton
+        onClick={onClick}
+      >
+        <Plus />
+        新增
+      </CustomButton>
+      <CreateDepartmentDialog
+        isOpen={isDialogOpen}
+        setIsOpen={setIsDialogOpen}
+        organizations={organizations}
       />
-    </CustomDialog>
+    </>
   )
 }
