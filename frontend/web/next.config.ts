9f5a972120907ffecf167a1157022f6d2b44520b
Hugo Mok <hugo.mok@infocast.com.hk>
2026-01-02 14:01:16 +0800

    Nextjs 16

--- frontend/web/next.config.ts
+++ frontend/web/next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
-
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Workaround for Vercel + Prisma + Monorepos (Multi app in 1 repo) issue (https://www.prisma.io/docs/orm/prisma-client/deployment/serverless/deploy-to-vercel)
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
 };
 
 export default nextConfig;
