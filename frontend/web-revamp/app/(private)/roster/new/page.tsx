d14e88e3da3f1c076d0f60d3de7111f33b9dacd5
Hugo Mok <hugo.mok@infocast.com.hk>
2026-01-05 12:33:02 +0800

    Breadcrumb title

--- frontend/web-revamp/app/(private)/roster/new/page.tsx
+++ frontend/web-revamp/app/(private)/roster/new/page.tsx
@@ -1,9 +1,20 @@
 import HeaderLayout from "@/components/_general/header/header-layout";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from "@/external/shadcn/components/ui/breadcrumb";
 
 export default function NewRosterPage() {
   return (
-    <HeaderLayout>
-      <div>NewRosterPage</div>
+    <HeaderLayout
+      title={(
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage>新增值班表</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      )}
+    >
+      <div></div>
     </HeaderLayout>
   )
 }
