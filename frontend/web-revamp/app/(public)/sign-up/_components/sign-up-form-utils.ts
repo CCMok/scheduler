ce73f85c8b5a12b20c260481d3fd84c225c60ee1
Hugo Mok <hugo.mok@infocast.com.hk>
2026-03-31 18:00:12 +0800

    Add validation on confirm password

--- frontend/web-revamp/app/(public)/sign-up/_components/sign-up-form-utils.ts
+++ frontend/web-revamp/app/(public)/sign-up/_components/sign-up-form-utils.ts
@@ -15,4 +15,7 @@ export const formSchema = z.object({
   [FORM_FIELD.PASSWORD]: z.string().min(1, Message.REQUIRED),
   [FORM_FIELD.CONFIRM_PASSWORD]: z.string().min(1, Message.REQUIRED),
   [FORM_FIELD.NAME]: z.string(),
+}).refine((data) => data[FORM_FIELD.PASSWORD] === data[FORM_FIELD.CONFIRM_PASSWORD], {
+  error: '確認密碼' + Message.INCORRECT,
+  path: [FORM_FIELD.CONFIRM_PASSWORD],
 })
