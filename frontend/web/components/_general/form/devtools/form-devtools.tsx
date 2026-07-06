b799325d1b763dad85b9afbb36a2a85dd2cb309b
Hugo Mok <hugomok0920@gmail.com>
2026-07-07 00:24:55 +0800

    Fix production should hide form devtool

--- frontend/web/components/_general/form/devtools/form-devtools.tsx
+++ frontend/web/components/_general/form/devtools/form-devtools.tsx
@@ -4,6 +4,9 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
 
 export default function FormDevtools() {
+  if (process.env.NODE_ENV === 'production' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'production')
+    return <></>;
+
   return (
     <TanStackDevtools plugins={[formDevtoolsPlugin()]} />
   )
